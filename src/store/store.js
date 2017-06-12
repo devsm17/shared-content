90be6568ddd49bc3f981a299f581934e25cd599e
omarou666 <omar.mohand.amer@gmail.com>
2017-06-12 10:33:22 +0200

    initial commit

---
+++ src/store/store.js
@@ -0,0 +1,20 @@
+import Vue from 'vue';
+import Vuex from 'vuex';
+
+import * as actions from './actions';
+import * as mutations from './mutations';
+import * as getters from './getters';
+
+Vue.use(Vuex);
+
+export const store = new Vuex.Store({
+  state: {
+
+  },
+  getters,
+  mutations,
+  actions,
+  modules: {
+    
+  }
+});
